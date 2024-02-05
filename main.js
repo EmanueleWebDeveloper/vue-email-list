2f2457c6d893b1abfa4a3a8ec8ef2a32f0ce4f06
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-02-05 17:31:07 +0100

    10 mail generate casualmente

--- main.js
+++ main.js
@@ -8,31 +8,33 @@ Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
 
-const {
-    createApp
-} = Vue
-createApp({
+const { createApp } = Vue;
+
+const app = createApp({
     data() {
         return {
-            mailRandom: 0,
+            mailRandom: [],
             
         }
 
     },
     mounted() {
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((result) => {
+        this.generaMailRandom();
 
-                console.log(result.data.response)
-                this.mailRandom = result.data.response
-
-            })
     },
     methods: {
-        
-    }
-},).mount('#app')
+        generaMailRandom() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((result) => {
+                        console.log(result.data.response);
+                        this.mailRandom.push(result.data.response);
+                    });
+            } 
+        },
+    },
+}).mount('#app');
 
 
 
